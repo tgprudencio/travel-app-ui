487b5f3fe9a7e4f23b143062720bbc63bf75634b
tgprudencio <thiago.gprudencio@gmail.com>
2022-08-26 20:26:41 -0300

    feat: finished Dashboard

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -10,14 +10,19 @@ import {
     Platform,
     StyleSheet
 } from 'react-native';
-
 import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants';
+import { TextButton } from '../components';
 
 const COUNTRIES_ITEM_SIZE = SIZES.width / 3;
+const PLACES_ITEM_SIZE = Platform.OS === 'ios' ? SIZES.width / 1.25 : SIZES.width / 1.2;
+const EMPTY_ITEM_SIZE = (SIZES.width - PLACES_ITEM_SIZE) / 2;
 
 const Dashboard = ({ navigation }) => {
-    const [countries, setCountries] = useState([ { id: -1 }, ...dummyData.countries, { id: -2 }]);
     const countryScrollX = useRef(new Animated.Value(0)).current;
+    const placesScrollX = useRef(new Animated.Value(0)).current;
+    const [countries, setCountries] = useState([ { id: -1 }, ...dummyData.countries, { id: -2 }]);
+    const [places, setPlaces] = useState([ { id: -1 }, ...dummyData.countries[0].places, { id: -2 }]);
+    const [placesScrollPosition, setPlacesScrollPosition] = useState(0);
 
     function renderHeader() {
         return (
@@ -55,6 +60,12 @@ const Dashboard = ({ navigation }) => {
                         contentOffset: { x: countryScrollX }
                     } }
                 ], { useNativeDriver: false }) }
+                onMomentumScrollEnd = { (event) => {
+                    // Calculate position
+                    var position = (event.nativeEvent.contentOffset.x / COUNTRIES_ITEM_SIZE).toFixed(0);
+                    // Set place
+                    setPlaces([ { id: -1 }, ...dummyData.countries[position].places, { id: -2 } ]);
+                } }
                 renderItem = {( { item, index } ) => {
                     const opacity = countryScrollX.interpolate({
                         inputRange: [
@@ -127,6 +138,133 @@ const Dashboard = ({ navigation }) => {
         );
     }
 
+    function exploreButtonHandler() {
+        // Get places current index
+        const currentIndex = parseInt(placesScrollPosition, 10) + 1;
+        // Navigate to the next screen
+        console.log(places[currentIndex]);
+        navigation.navigate('Place', { selectedPlace: places[currentIndex] });
+    }
+
+    function renderPlaces() {
+        return (
+            <Animated.FlatList
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator = { false }
+                data = { places }
+                keyExtractor = { item => `${item.id}` }
+                contentContainerStyle = {{ 
+                    alignItems: 'center'
+                }}
+                snapToAlignment = 'center'
+                snapToInterval=  { Platform.OS === 'ios' ? PLACES_ITEM_SIZE + 28 : PLACES_ITEM_SIZE }
+                scrollEventThrottle = { 16 }
+                decelerationRate = { 0 }
+                bounces = { false }
+                onScroll = { Animated.event([
+                    { nativeEvent: { contentOffset: { x: placesScrollX } } }
+                ], { useNativeDriver: false }) }
+                onMomentumScrollEnd = { (event) => {
+                    // Calculate position
+                    var position = (event.nativeEvent.contentOffset.x / PLACES_ITEM_SIZE).toFixed(0);
+                    // Set Place Scroll position
+                    setPlacesScrollPosition(position);
+                }}
+                renderItem = { ({ item, index }) => {
+
+                    const opacity = placesScrollX.interpolate({
+                        inputRange: [
+                            (index - 2) * PLACES_ITEM_SIZE,
+                            (index - 1) * PLACES_ITEM_SIZE,
+                            index * PLACES_ITEM_SIZE
+                        ],
+                        outputRange: [0.3, 1, 0.3],
+                        extrapolate: 'clamp'
+                    });
+                    let activeHeight = 0;
+
+                    if (Platform.OS === 'ios') {
+                        if (SIZES.height > 800) {
+                            activeHeight = SIZES.height / 2;
+                        } else {
+                            activeHeight = SIZES.height / 1.65;
+                        }
+                    } else {
+                        activeHeight = SIZES.height / 1.6;
+                    }
+
+                    const height = placesScrollX.interpolate({
+                        inputRange: [
+                            (index - 2) * PLACES_ITEM_SIZE,
+                            (index - 1) * PLACES_ITEM_SIZE,
+                            index * PLACES_ITEM_SIZE
+                        ],
+                        outputRange: [SIZES.height / 2.25, activeHeight, SIZES.height / 2.25],
+                        extrapolate: 'clamp'
+                    });
+
+                    if (index  == 0 || index == places.length - 1) {
+                        return (
+                            <View style = {{ width: EMPTY_ITEM_SIZE }}>
+
+                            </View>
+                        );
+                    } else {
+                        return (
+                            <Animated.View
+                                opacit = { opacity }
+                                style = {{
+                                    width: PLACES_ITEM_SIZE,
+                                    height: height,
+                                    alignItems: 'center',
+                                    borderRadius: 20,
+                                    padding: 10
+                                }}
+                            >
+                                <Image 
+                                    source = { item.image }
+                                    resizeMode = 'cover'
+                                    style = {{
+                                        position: 'absolute',
+                                        width: '100%',
+                                        height: '100%',
+                                        borderRadius: 20
+                                    }}
+                                />
+
+                                <View
+                                    style = {{
+                                        flex: 1,
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-end',
+                                        marginHorizontal: SIZES.padding
+                                    }} 
+                                >
+                                    <Text style = {{ marginBottom: SIZES.radius, color: COLORS.white, ... FONTS.h1 }}>{ item.name }</Text>
+                                    <Text style = {{ marginBottom: SIZES.padding * 2, textAlign: 'center', color: COLORS.white, ... FONTS.body3 }}>{ item.description }</Text>
+                                    <TextButton  
+                                        label = 'Explore'
+                                        customContainerStyle = {{
+                                            position: 'absolute',
+                                            bottom: -20,
+                                            width: 150
+                                        }}
+                                        onPress = { () => exploreButtonHandler() }
+                                    />
+                                </View>
+                            </Animated.View>
+                        );
+                    }
+
+                    
+                } }
+            >
+
+            </Animated.FlatList>
+        );
+    }
+
     return (
         <SafeAreaView style = {{ flex: 1, backgroundColor: COLORS.darkGreen }}>
            { renderHeader() }
@@ -135,6 +273,9 @@ const Dashboard = ({ navigation }) => {
                     { /* Countries */ }
                     <View>
                         { renderCountries() }
+                    </View>
+                    <View style={{ height: Platform.OS === 'ios' ? 500 : 450 }}>
+                        {  renderPlaces() } 
                     </View>
                 </View>
            </ScrollView>
